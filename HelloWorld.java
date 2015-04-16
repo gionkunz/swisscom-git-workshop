7ecdf0b97e29b5a84c5da4841b28bfe66674814c
Gion Kunz <gion.kunz@gmail.com>
2015-04-16 15:45:36 +0200

    American style

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
     private int num = 100;
 
     public static void main(String[] args) {
-        System.out.println("Hi World  dd!");
+        System.out.println("Howdy World!");
     }
 }
 
